305d9a5c2ccf0b3007825bde0260abd9dc2471e2
daniloradovic <mailman.danjo@gmail.com>
2017-05-06 22:16:31 +0200

    Basic solution

--- lib/race.rb
+++ lib/race.rb
@@ -3,8 +3,25 @@ module RaceBet
 
     class << self
       def score(guesses, winners)
-
+      	sum = 0
+      	guesses.each_with_index do |guess, index|
+      		if index == 0 && guess == winners[index]
+      			sum += 15
+      		elsif index == 1 && guess == winners[index]
+      			sum += 10
+      		elsif index == 2 && guess == winners[index]
+      			sum += 5
+      		elsif index == 3 && guess == winners[index]
+      			sum += 3
+      		elsif index == 4 && guess == winners[index]
+      			sum += 1
+      		elsif winners[0..4].include?(guess)
+      			sum += 1
+      		end
+      	end	
+      	sum
       end
+
     end
 
   end
